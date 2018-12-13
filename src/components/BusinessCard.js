515966a78fa9ae46e52395c3b3d99478cec431e8
elpoto <1258870+elbotho@users.noreply.github.com>
2018-12-13 17:09:52 +0100

    Revert "wip: link to plan route"
    This reverts commit 2bf87eaa0f2a288f0d7918439faa4d21ebce79d1.

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -86,7 +86,6 @@ class BusinessCard extends Component {
     }
     else {
       const shortHomepage = entry.homepage ? entry.homepage.replace("http://", "").replace("https://", "").replace("www.", "") : ""
-      const routeUrl = 'https://maps.openrouteservice.org/directions?a=null,null,' + entry.lat +","+ entry.lng
       return (
         <EntryDetailPage hasImage={hasImage}>
           <EntryTitle>{entry.title}</EntryTitle>
@@ -128,9 +127,6 @@ class BusinessCard extends Component {
               ? Tags(entry.tags)
               : null)
           ]}</EntryDetailsOtherData>
-          <a href={routeUrl} target="_blank" style={{display: "none"}}>Routenplaner</a>
-          
-
         </EntryDetailPage>)
     }
   }
